e7b3213cfd3077108b080777c5e2c7c8bab28946
rrobaina <rrobaina@antel.net.uy>
2020-07-02 16:25:22 -0300

    we broke homepage into components and add react-router-dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div><HomePage></HomePage></div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/hats" component={HatsPage}/>
+      </Switch>
+    </div>
   );
 }
 
